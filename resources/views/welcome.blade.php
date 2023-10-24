2933c6c0e9600b299d0379d061367f431d48fadf
Khoirusy Syafaat <syafaatkhoirusy@gmail.com>
2023-10-24 09:12:41 +0700

    hidden register dan ganti judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,16 +73,16 @@
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
+                        {{-- @if (Route::has('register'))
                             <a href="{{ route('register') }}">Register</a>
-                        @endif
+                        @endif --}}
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Admin CRUD
+                    Pemenangan Kms.H.M. Umar Halim
                 </div>
             </div>
         </div>
