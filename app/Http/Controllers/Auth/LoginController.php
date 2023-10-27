aab6aa45751375f56e97b058d3fc477f4ba466a8
Khoirusy Syafaat <syafaatkhoirusy@gmail.com>
2023-10-27 16:36:36 +0700

    dashboard redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 
     use AuthenticatesUsers;
 
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin/dashboard';
 
     public function __construct()
     {
