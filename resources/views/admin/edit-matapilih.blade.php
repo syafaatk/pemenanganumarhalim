c74ebf1ac5760833e48217410202f3ec51758b2c
Khoirusy Syafaat <syafaatkhoirusy@gmail.com>
2023-11-01 09:33:36 +0700

    update

--- resources/views/admin/edit-matapilih.blade.php
+++ resources/views/admin/edit-matapilih.blade.php
@@ -17,6 +17,7 @@
           <label for="admin">Admin</label>
           <input type="text" disabled class="form-control" value="{{ $matapilih->user->name }}">
           <input name="user_id" type="hidden" class="form-control" id="admin" value="{{ Auth::user()->id }}" required>
+          <input name="is_manual" type="hidden" class="form-control" value="{{ $matapilih->is_manual }}" required>
         </div>
         @endif
         @if(Auth::user()->super_admin == "1")
