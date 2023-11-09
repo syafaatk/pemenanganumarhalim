0d9b4e436143459c29a852a98aa6ee607fe8c7a8
Khoirusy Syafaat <syafaatkhoirusy@gmail.com>
2023-11-09 10:12:14 +0700

    update

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,90 @@
+<?php
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Http\Request;
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('user.index', compact('users'));
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('user.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        User::create($request->all());
+        $msg = "New User Created successful! ";
+        return redirect('user')->with('msg', $msg);
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        return view('user.show', compact('user'));
+    }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('user.edit', compact('user'));
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $update = [
+            "name"=>$request->name,
+            "email"=>$request->email,
+            "password"=>$request->password,
+        ];
+        User::where('id', $id)->update($update);
+        $msg = "User Updated successful! ";
+        return redirect('user')->with('msg', $msg);
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::destroy($id);
+        $msg = "User Deleted successful! ";
+        return redirect('user')->with('msg', $msg);
+    }
+} 
