81a39d22994e0a803b8441b9317337a11154d8e9
Khoirusy Syafaat <syafaatkhoirusy@gmail.com>
2023-11-09 16:48:28 +0700

    update

--- resources/views/admin/create-matapilih.blade.php
+++ resources/views/admin/create-matapilih.blade.php
@@ -125,7 +125,7 @@
         }
         // console.log('id', $("#nik").val());
         $.ajax({
-            url: `https://indonesian-identification-card-ktp.p.rapidapi.com/api/v3/check?nik=${nik}`,
+            url: `#`,
             headers: {
                 "X-RapidAPI-Key": "{{ config('app.API_Key')}}",
                 "X-RapidAPI-Host": "{{ config('app.API_Host')}}"
